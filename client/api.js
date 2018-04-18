699637bb3f8978389084ef1fc9793d9751064270
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-04-18 16:04:10 +1200

    hhj

--- client/api.js
+++ client/api.js
@@ -2,6 +2,8 @@ import request from 'superagent'
 
 const issUrl = 'https://api.wheretheiss.at/v1/satellites'
 
+const nasaURL = 'https://api.nasa.gov/planetary/apod?api_key=hVgphNsE2IErmoVL7M09ckfwvySkSsTFgA7pdfGk'
+
 export function getSatelliteById (callback) {
    request
     .get(issUrl+'/25544')
@@ -10,3 +12,8 @@ export function getSatelliteById (callback) {
       callback(err, res.body)
     })
 }
+
+
+
+
+//https://api.nasa.gov/planetary/apod?api_key=hVgphNsE2IErmoVL7M09ckfwvySkSsTFgA7pdfGk
