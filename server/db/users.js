a4c7d3a390aa7c2bdf80b6b007089f416045d779
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-04-30 16:17:45 +1200

    env added to gitignore

--- server/db/users.js
+++ server/db/users.js
@@ -20,7 +20,7 @@ function userExists (username, conn) {
   const db = conn || connection
   return db('users')
     .count('id as n')
-    .where('username' = username)
+    .where('username', username)
     .then(count => {
       return count[0].n > 0
     })
